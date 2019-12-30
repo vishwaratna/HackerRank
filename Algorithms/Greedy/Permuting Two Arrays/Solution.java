72147015d5c2a230ad4034dd0beab288a5130f72
Vishwa Ratna <visvishwa@gmail.com>
2019-12-30 11:35:25 +0530

    A simple Solution in JAVA-8

--- Algorithms/Greedy/Permuting Two Arrays/Solution.java
+++ Algorithms/Greedy/Permuting Two Arrays/Solution.java
@@ -34,43 +34,12 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int Q = input.nextInt();
-        //Store answer to each query
-        StringBuilder output = new StringBuilder("");
-
-        queries:
-        for(int q = 0; q < Q; q++)
-        {
-            int n = input.nextInt();
-            int k = input.nextInt();
-            
-            //Initialize Input
-            int[] A = new int[n];
-            int[] B= new int[n];
-            for(int i = 0; i < n; i++)
-                A[i] = input.nextInt();
-            for(int i = 0; i < n; i++)
-                B[i] = input.nextInt();
-            
-            Arrays.sort(A);//Sort ascending
-            Arrays.sort(B);//Sort acending      
-          
-            for(int i = 0; i < n; i++)
-            {
-                //Traverse A ascending and B descending
-                if(A[i]+B[B.length-1-i] < k) //Failed check
-                {
-                    output.append("NO\n");
-                    continue queries;
-                }
-            }
-            
-            //Permutation exist
-            output.append("YES\n");
-            
+        
+        int[] ints = Arrays.stream(A).boxed().sorted(Comparator.reverseOrder()).mapToInt(i -> i).toArray();
+        int[] ints1 = Arrays.stream(B).boxed().sorted(Comparator.naturalOrder()).mapToInt(i -> i).toArray();
+        final long count = IntStream.rangeClosed(0, A.length - 1).filter(i -> ints[i] + ints1[i] >=k).count();
+        if(count== A.length){
+            System.out.println("YES");
         }
-        //Print the answers to all queries
-        System.out.println(output);
     }
 }
